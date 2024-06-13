4b33b84d1d7239a2e10661936492a2a0441dfa24
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-13 15:46:03 +0900

    基本的なView等作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :correct_user, only: [:show]
+  skip_before_action :login_required, only: [:new, :create]
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # ユーザ登録に成功した場合の処理
+      log_in(@user)
+      redirect_to task_path(@user.id)
+    else
+      # ユーザ登録に失敗した場合の処理
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  #この下に書くなアホ
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to current_user unless current_user?(@user)
+  end
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  include SessionsHelper
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:alert] = "ログインが必要です"
+      redirect_to login_path
+    end
+  end
 end
